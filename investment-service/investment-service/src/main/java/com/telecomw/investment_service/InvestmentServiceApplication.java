f0c1096dda39dbf6d7edaedd9715753b7ada8a90
astitva.singh <astitmini@gmail.com>
2026-02-16 14:32:55 +0530

    base infra setup

---
+++ investment-service/investment-service/src/main/java/com/telecomw/investment_service/InvestmentServiceApplication.java
@@ -0,0 +1,15 @@
+package com.telecomw.investment_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@EnableFeignClients
+@SpringBootApplication
+public class InvestmentServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvestmentServiceApplication.class, args);
+	}
+
+}
---
+++ telecom-usage-service/telecom-usage-service/src/test/java/com/telecomw/telecom_usage_service/TelecomUsageServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.telecomw.telecom_usage_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelecomUsageServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
